87762ed99b3115a72edc1a8d7dd8b3fcc32f122e
cristomane <101989028+cristomane@users.noreply.github.com>
2026-04-02 01:27:37 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -77,7 +77,7 @@ function App() {
   }, []);
 
   return (
-    <Router basename="/raylink-site">
+    <Router basename="/">
       <div className="min-h-screen bg-background text-foreground overflow-x-clip transition-colors duration-300">
         <Routes>
           <Route path="/" element={<MainPage />} />
